43def13b0dc12d8b4fc1d3bee42df235e23c6d91
mersalhussin <mersalhussin@gmail.com>
2024-06-07 16:56:42 +0300

    Upload Files

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './mood.css'
 
 function App() {
   const [name , setName] = useState("Mersal") 
   const [age , setAge] = useState(20) 
   const [count , setCount] = useState(0)
+  const [mood , setMood] = useState("light")
   
   const nameChange = () => {
   if (name == "Mersal" ){
@@ -22,17 +24,25 @@ function App() {
     setCount(count+1);
   }
   return (
-    <>
-      <h1>Hello {name}</h1>
-      <button onClick={nameChange}>Click Me</button>
-      <h1>My Age Is {age}</h1>
-      <button onClick={() => {setAge(21)}}>Click Me</button>
-      <br />
-      <br />
-      <br />
-      <br />
-      <button onClick={counter}>Count Is {count}</button>
-    </>
+    <div className={`App ${mood}`}>
+      <div className='buttons'>
+        <button  onClick={() => setMood("")}>Light</button>
+        <button onClick={() => setMood("dark")}>Dark</button>
+        <button onClick={() => setMood("pink")}>Pink</button>
+        <button onClick={() => setMood("gray")}>Gray</button>
+        <button onClick={() => setMood("green")}>Green</button>
+        <h1>Hello {name}</h1>
+        <button onClick={nameChange}>Click Me</button>
+        <h1>My Age Is {age}</h1>
+        <button onClick={() => {setAge(21)}}>Click Me</button>
+        <br />
+        <br />
+        <br />
+        <br />
+        <button onClick={counter}>Count Is {count}</button>
+      </div>
+    </div>
+
   )
 }
 
