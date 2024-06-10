39da977d57e475042778cb97ee7e54bc08ce2488
mersalhussin <mersalhussin@gmail.com>
2024-06-10 22:44:43 +0300

    Upload Files With not good task

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './mood.css'
-import Toggle from '../components/Toggle'
+import { useReducer } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./mood.css";
+import Toggle from "../components/Toggle";
+
+const initialData = {
+  name: "Mersal",
+  age: 20,
+  startCount: 0,
+  theme: "light",
+  mood: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_NAME":
+      return { ...state, name: action.newValue };
+    case "CHANGE_AGE":
+      return { ...state, age: action.newValue };
+    case "COUNTER":
+      return { ...state, startCount: action.newValue };
+    case "TOGGLE":
+      return { ...state, theme: state.theme == "light" ? "dark" : "light" };
+    case "TOGGLE_N":
+      return { ...state, mood: action.newValue};
+    case "TOGGLE_DARK":
+      return { ...state, mood: action.newValue};
+    case "TOGGLE_PINK":
+      return { ...state, mood: action.newValue};
+    case "TOGGLE_GREEN":
+      return { ...state, mood: action.newValue};
+    case "TOGGLE_GREEN":
+      return { ...state, mood: action.newValue};
+    case "TOGGLE_GRAY":
+      return { ...state, mood: action.newValue};
+
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [name , setName] = useState("Mersal") 
-  const [age , setAge] = useState(20) 
-  const [count , setCount] = useState(0)
-  const [mood , setMood] = useState("light")
-  const [toggle , settoggle] = useState("")
-  
-  const nameChange = () => {
-  if (name == "Mersal" ){
-      setName("Hussin")
-  }else{
-    setName("Mersal")
-  }
-}
-/*
-  -------------------------
-  --Toggle Menu Condation--
-  -------------------------
-*/
-const toggleFun = () => {
-  (toggle == "") ? settoggle("dark")  : settoggle("")
-}
+  const [allData, dispatch] = useReducer(reducer, initialData);
 
-  const counter = () => {
-    setCount(count+1);
-  }
   return (
-    <div className={`App ${toggle}`}>
-        <button  style={{margin:30}} onClick={toggleFun}>Toggle Menu</button>
-      {/* <div className='buttons'>
-        <button  onClick={() => setMood("")}>Light</button>
-        <button onClick={() => setMood("dark")}>Dark</button>
-        <button onClick={() => setMood("pink")}>Pink</button>
-        <button onClick={() => setMood("gray")}>Gray</button>
-        <button onClick={() => setMood("green")}>Green</button>
-      </div> */}
-      <Toggle func={toggleFun}/>
-        <h1>Hello {name}</h1>
-        <button onClick={nameChange}>Click Me</button>
-        <h1>My Age Is {age}</h1>
-        <button onClick={() => {setAge(21)}}>Click Me</button>
-        <br />
-        <br />
-        <br />
-        <br />
-        <button onClick={counter}>Count Is {count}</button>
-    </div>
+    <div className={`App ${allData.mood}`}>
+      <button onClick={()=>{
+        dispatch({ type: "TOGGLE", newValue: "dark" });
+      }}>Toggle Menu</button>
+      <div className='buttons'>
+        <button  onClick={() =>{
+          dispatch({ type: "TOGGLE_N", newValue: "" })
+        }}>Light</button>
+        <button onClick={() => {
+          dispatch({ type: "TOGGLE_DARK", newValue: "dark" })
+        }}>Dark</button>
+        <button onClick={() => {
+          dispatch({ type: "TOGGLE_PINK", newValue: "pink" })
+        }}>Pink</button>
+        <button onClick={() => {
+          dispatch({ type: "TOGGLE_GREEN", newValue: "green" })
+        }}>Green</button>
+        <button onClick={() => {
+          dispatch({ type: "TOGGLE_GRAY", newValue: "gray" })
+        }}>Gray</button>
+      </div>
 
-  )
+      <h1>Hello {allData.name}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "CHANGE_NAME", newValue: "Hussin" });
+        }}
+      >
+        Click Me
+      </button>
+      <h1>My Age Is {allData.age}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "CHANGE_AGE", newValue: "21" });
+        }}
+      >
+        Click Me
+      </button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          dispatch({ type: "COUNTER", newValue:(allData.startCount + 1) });
+        }}
+      >
+        Count Is {allData.startCount}{" "}
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
